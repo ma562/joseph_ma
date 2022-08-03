3fa42a97fddc9637c535aa7de54613512ae6c4af
Joseph Ma <Josephma@JosephsAir3826.local>
2022-08-03 19:03:49 -0400

    home background testing pt 8

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -7,7 +7,7 @@ if (pick == 0){
 	id.style.backgroundImage = "url('images/glow_stark5.png')"
 }
 else{
-	id.style.backgroundImage = "url('images/back_test.png')"
+	id.style.backgroundImage = "url('images/back.png')"
 }
 
 
