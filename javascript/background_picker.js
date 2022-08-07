c6ae47d1b1c56c56a1c9760c5536838185dc75c8
Joseph Ma <Josephma@JosephsAir3826.local>
2022-08-07 02:13:47 -0400

    background 1

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -8,11 +8,11 @@ if (pick == 0){
 	id.style.backgroundImage = "url('images/glow_stark5.png')"
 }
 else{
-	id.style.backgroundImage = "url('images/back14.png')"
+	id.style.backgroundImage = "url('backgrounds/background_1.png')"
 }
 
 if (screen.width < 1300) {
-	id.style.backgroundImage = "url('images/mobile1.png')"
+	id.style.backgroundImage = "url('mobile_backgrounds/mobile_background_1.png')"
 }
 else {
 
