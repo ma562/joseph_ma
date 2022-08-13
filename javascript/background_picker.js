af4c9eedb22c63c7e82e67450bdb7c31e3364303
Joseph Ma <Josephma@JosephsAir3826.local>
2022-08-13 15:45:59 -0400

    background project 6 update

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -7,7 +7,7 @@ let id = document.getElementById("home")
 
 if (screen.width >= 1300) {
 
-	bkground = "url('backgrounds/background_" + String(24) + ".png')"
+	bkground = "url('backgrounds/background_" + String(pick) + ".png')"
 	id.style.backgroundImage = bkground
 
 
