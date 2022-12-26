0af6bfb22689787048540cb38d0a4953a60dcbfd
Joseph Ma <Josephma@Josephs-MacBook-Air-3826.local>
2022-12-25 22:10:34 -0500

    memories section initial drop down bar

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -1,21 +1,33 @@
-NUM_BACKGROUNDS = 28
-pick = Math.floor(Math.random() * NUM_BACKGROUNDS) + 1
+alert("Joseph's currently working on a memories section of his website. It's not working at the moment but will be up by the beginning of 2023. Click Ok to continue to site.")
 
+const my_background = localStorage.getItem('my__background');
+console.log(my_background)
 
 let id = document.getElementById("home")
 
+if(my_background == null) {
+	//we did not previously select a background --> randomly generate one
+	NUM_BACKGROUNDS = 28
+	pick = Math.floor(Math.random() * NUM_BACKGROUNDS) + 1
 
-if (screen.width >= 1300) {
+	if (screen.width >= 1300) {
 
-	bkground = "url('backgrounds/background_" + String(pick) + ".png')"
-	id.style.backgroundImage = bkground
+		bkground = "url('backgrounds/background_" + String(pick) + ".png')"
+		id.style.backgroundImage = bkground
+	}
+	else {
+		bkground = "url('mobile_backgrounds/mobile_background_" + String(pick) + ".png')"
+		id.style.backgroundImage = bkground
+	}
+
 }
 else {
-	bkground = "url('mobile_backgrounds/mobile_background_" + String(pick) + ".png')"
+	//memories background selector
+	bkground = "url('images/wordle.png')"
 	id.style.backgroundImage = bkground
 }
 
 id.style.backgroundSize = "cover";
 id.style.backgroundRepeat = "no-repeat"
 
-
+localStorage.removeItem('my__background');
