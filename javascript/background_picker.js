0bba46d558cb8b2900237c80a20c6d27b76858ae
Joseph Ma <Josephma@Josephs-MacBook-Air-3826.local>
2023-01-01 05:22:41 -0500

    fall 22

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -22,20 +22,36 @@ if(my_background == null) {
 }
 else {
 	//memories background selector
-	if(my_background > 0) {
-		pick = my_background
-		pick *= 4;
-		pick -= Math.floor(Math.random() * 4);
-	}
-	if (screen.width >= 1300) {
-		bkground = "url('backgrounds/background_" + String(pick) + ".png')"
+	if(typeof my_background == "string") {
+		//string --> MEMORIES section
+		if(screen.width >= 1300) {
+			bkground = "url('memories/" + my_background + ".png')"
+		}
+		else {
+			bkground = "url('mobile_memories/" + my_background + ".png')"
+		}
 		id.style.backgroundImage = bkground
 	}
 	else {
-		bkground = "url('mobile_backgrounds/mobile_background_" + String(pick) + ".png')"
+		//integer --> THEMES section
+		pick = my_background
+		pick *= 4;
+		pick -= Math.floor(Math.random() * 4);
+		if (screen.width >= 1300) {
+			bkground = "url('backgrounds/background_" + String(pick) + ".png')"
+		}
+		else {
+			bkground = "url('mobile_backgrounds/mobile_background_" + String(pick) + ".png')"
+		}
 		id.style.backgroundImage = bkground
 	}
 }
+
+//code change
+// bkground = "url('backgrounds/Base_Background.png')"
+// id.style.backgroundImage = bkground
+
+//here
 
 id.style.backgroundSize = "cover";
 id.style.backgroundRepeat = "no-repeat"
