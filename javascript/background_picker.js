d4d2d4bb8f969448888d5be5e559d546d153b321
Joseph Ma <Josephma@Josephs-MacBook-Air-3826.local>
2023-01-01 05:34:24 -0500

    replace typeof with isNan to check for int vs string

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -22,7 +22,7 @@ if(my_background == null) {
 }
 else {
 	//memories background selector
-	if(typeof my_background == "string") {
+	if(isNan(Number(my_background))) {
 		alert("it's a string!")
 		//string --> MEMORIES section
 		if(screen.width >= 1300) {
