d06ab405daf13d174ff4302eadb505b20508b77a
Joseph Ma <Josephma@pal-nat186-70-117.itap.purdue.edu>
2024-01-16 13:01:59 -0500

    shadows

--- javascript/background_picker.js
+++ javascript/background_picker.js
@@ -1,4 +1,27 @@
 // alert("Website is currently under maintenance - theme and memories section work in progress.")
+// function changeTextColor(colorForText1And3, colorForText2) {
+//     // Change color for text-1 and text-3
+//     document.querySelectorAll('.home .home-content .text-1, .home .home-content .text-3').forEach(function(el) {
+//         el.style.color = colorForText1And3;
+//     });
+
+//     // Change color for text-2
+//     document.querySelector('.home .home-content .text-2').style.color = colorForText2;
+// }
+
+// function changeTextColor(colorForText1And3, colorForText2) {
+//     // Apply a stronger shadow to text-1 and text-3
+//     document.querySelectorAll('.home .home-content .text-1, .home .home-content .text-3').forEach(function(el) {
+//         el.style.color = colorForText1And3; // White or neon green
+//         //el.style.textShadow = '3px 3px 6px #000000'; // Stronger black shadow
+//         el.style.textShadow = '4px 4px 8px #000000'; // Stronger black shadow
+//     });
+
+//     // Apply a stronger shadow to text-2
+//     document.querySelector('.home .home-content .text-2').style.color = colorForText2; // White or neon green
+//     document.querySelector('.home .home-content .text-2').style.textShadow = '3px 3px 6px #000000'; // Stronger black shadow
+// }
+	
 
 const my_background = localStorage.getItem('my__background');
 const is_theme = localStorage.getItem('is__theme')
@@ -40,6 +63,7 @@ else {
 		pick *= 4;
 		pick -= Math.floor(Math.random() * 4);
 		if (screen.width >= 1300) {
+			console.log(pick)
 			bkground = "url('backgrounds/background_" + String(pick) + ".png')"
 		}
 		else {
@@ -55,12 +79,17 @@ else {
 			bkground = "url('mobile_memories/mobile_" + my_background + ".png')"
 		}
 		id.style.backgroundImage = bkground
+
+		// if (my_background === "fall23" && screen.width >= 1300) {
+		// 	// adapt text color to background
+		// 	changeTextColor('#FFFFFF', '#39FF14');
+		// }
 	}
 }
 
 //remove the localStorage so if page is refreshed we go back to homepage
-localStorage.removeItem('my__background');
-localStorage.removeItem('is__theme');
+// localStorage.removeItem('my__background');
+// localStorage.removeItem('is__theme');
 
 id.style.backgroundSize = "cover";
 id.style.backgroundRepeat = "no-repeat"
